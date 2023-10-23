3269f40881fd48e9dff2aff7b0a6a24393ac8d5e
Stephan Meesters <stephan.meesters@gmail.com>
2023-10-23 14:34:25 +0200

    various improvements

--- content.js
+++ content.js
@@ -1,26 +1,128 @@
-function checkElement(selector) {
+setTimeout(scanAndAddCommitCopyElement(), 100);
+
+function scanAndAddCommitCopyElement() {
+    const template = getTemplate();
+    let keyboardListener;
+    let cbrowser = chrome ?? browser;
+    checkElement(template.issueBody).then(async (element) => {
+        const newElement = document.createElement("div");
+        newElement.className = 'yt-enhancer-row';
+
+        const inputElement = document.createElement("div");
+        const inputIdName = "yt-enhancer-input-peer";
+        inputElement.innerHTML = `Peer: <input id=${inputIdName} style="width: 30px"/>`;
+        newElement.appendChild(inputElement);
+
+        const copyButton = generateButton();
+        newElement.appendChild(copyButton);
+
+        let copyFunc = async () => {
+            try {
+                const innerIdElement = await checkElement(template.issueId);
+                const issueId = innerIdElement.innerText;
+
+                const innerIssueElement = await checkElement(template.issueName);
+                const issueName = innerIssueElement.innerText;
+
+                const peer = document.getElementById(inputIdName).value.toUpperCase();
+                let txt = `${issueName} [Peer: ${peer}]\nIssue: ${issueId}`;
+
+                await copyToClipboard(txt);
+            } catch (error) {
+                console.info(error);
+            }
+        };
+        keyboardListener = (command) => {
+            if (command === "copy-commit-to-clipboard") {
+                copyFunc();
+            }
+        };
+
+        copyButton.onclick = copyFunc;
+        cbrowser?.commands.onCommand.addListener(keyboardListener);
+        element.parentNode.insertBefore(newElement, element);
+
+        const observer = new MutationObserver(function (_mutations) {
+            if (!document.contains(newElement)) {
+                observer.disconnect();
+                if (cbrowser?.commands.onCommand.hasListener(keyboardListener)) {
+                    cbrowser?.commands.onCommand.removeListener(keyboardListener);
+                }
+                scanAndAddCommitCopyElement();
+            }
+        });
+        observer.observe(document.body, { childList: true, subtree: true });
+    })
+        .catch((error) => {
+            console.info(error);
+            if (cbrowser?.commands.onCommand.hasListener(keyboardListener)) {
+                cbrowser?.commands.onCommand.removeListener(keyboardListener);
+            }
+            scanAndAddCommitCopyElement();
+        });
+}
+
+function getTemplate() {
+    if (isLiteMode()) {
+        if (window.location && window.location.href.includes('/agiles/')) {
+            return {
+                issueBody: "yt-issue-body",
+                issueId: ".js-issue-id",
+                issueName: ".yt-issue-body__summary.yt-issue-body__summary-common.yt-vgutter-bottom-2"
+            }
+        }
+        else {
+            return {
+                issueBody: "span.highlighter__d45",
+                issueId: "span.ring-ui-inner_e3ba.__singleValue__",
+                issueName: `h1[data-test="ticket-summary"]`
+            }
+        }
+    }
+    else {
+        return {
+            issueBody: "yt-issue-body",
+            issueId: ".js-issue-id",
+            issueName: ".yt-issue-body__summary.yt-issue-body__summary-common.yt-vgutter-bottom-2"
+        }
+    }
+}
+
+function isLiteMode() {
+    if (document.body.classList.contains("global_simplified-ui")) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function checkElement(selector, timeout = 1000) {
     return new Promise((resolve, reject) => {
         const element = document.querySelector(selector);
         if (element !== null) {
             resolve(element);
+            return;
         }
-        else {
-            const observer = new MutationObserver((mutations, me) => {
-                const element = document.querySelector(selector);
-                if (element !== null) {
-                    me.disconnect();
-                    resolve(element);
-                }
-            });
 
-            observer.observe(document, {
-                childList: true,
-                subtree: true
-            });
-        }
+        const observer = new MutationObserver((_mutations, me) => {
+            const element = document.querySelector(selector);
+            if (element !== null) {
+                me.disconnect();
+                resolve(element);
+            }
+        });
+
+        observer.observe(document, {
+            childList: true,
+            subtree: true
+        });
+
+        setTimeout(() => {
+            observer.disconnect();
+            reject(new Error(`Element ${selector} was not found within ${timeout}ms`));
+        }, timeout);
     });
 }
-
 function generateButton() {
     const button = document.createElement("button");
 
@@ -57,39 +159,3 @@ async function copyToClipboard(textToCopy) {
         }
     }
 }
-
-checkElement("yt-issue-body").then(async (element) => {
-    const newElement = document.createElement("div");
-    newElement.className = 'yt-enhancer-row';
-
-    var innerIdElement = await checkElement(".js-issue-id");
-    var issueId = innerIdElement.innerText;
-
-    var innerIssueElement = await checkElement(".yt-issue-body__summary.yt-issue-body__summary-common.yt-vgutter-bottom-2");
-    var issueName = innerIssueElement.innerText;
-
-    var inputElement = document.createElement("div");
-    inputElement.innerHTML = `Peer: <input id="yt-enhancer-input-peer" style="width: 30px"/>`;
-    newElement.appendChild(inputElement);
-
-    var copyButton = generateButton();
-    copyButton.onclick = async () => {
-        var peer = document.getElementById("yt-enhancer-input-peer").value.toUpperCase();
-        var txt;
-        if (peer.length > 0) {
-            txt = `${issueName} [Peer: ${peer}]\nIssue: ${issueId}`;
-        }
-        else {
-            txt = `${issueName}\nIssue: ${issueId}`;
-        }
-
-        try {
-            await copyToClipboard(txt);
-        } catch (error) {
-            console.error(error);
-        }
-    };
-    newElement.appendChild(copyButton);
-
-    element.parentNode.insertBefore(newElement, element);
-});
